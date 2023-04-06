fe6ad8c24ee98472f197cc7caf4ac701760706af
alikadev <dev@alikadev.com>
2023-04-06 22:21:23 +0200

    Initial Commit

---
+++ MoneyMan/MoneyManApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyManApp.swift
+//  MoneyMan
+//
+//  Created by Alikadev on 06.04.23.
+//
+
+import SwiftUI
+
+@main
+struct MoneyManApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
